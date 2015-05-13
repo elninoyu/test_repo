dc71e5ae06d153a5d0c748c4b7f46b8b0f34524e
elninoyu <shift_david@msn.com>
2015-05-13 13:43:00 -0400

    add testfolder

---
+++ testfolder/src/temp_functions.py
@@ -0,0 +1,11 @@
+
+def k_to_c(temp):
+    return temp-273.15
+
+def f_to_k(temp):
+    return (temp-32.0)*5.0/9.0+273.15
+
+def f_to_c(temp):
+    for x in range(100):
+        print "ALL WORK AND NO PLAY MAKES JACK A DULL BOY "
+
